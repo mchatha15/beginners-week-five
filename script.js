1410b7269bdedde128a1336a2862039400cf9728
mchatha15 <56553140+mchatha15@users.noreply.github.com>
2019-11-18 19:59:51 +0000

    add javascript file

---
+++ script.js
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  </head>
+  <body>
+    <input id="input" type="text">
+    <button id="button">Add</button>
+    <ul id="list">
+      <li>Example</li>
+    </ul>
+  </body>
+</html>
